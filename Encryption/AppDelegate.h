653f7d4660ed14b0caa8d85ae7989b3cb4b394a1
150_3112006154_梁展焯 <569676974@qq.com>
2016-11-05 12:44:42 +0800

    项目初始化

---
+++ Encryption/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Encryption
+//
+//  Created by 粱展焯 on 16/10/21.
+//  Copyright © 2016年 梁展焯. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Encryption/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Encryption
+//
+//  Created by 粱展焯 on 16/10/21.
+//  Copyright © 2016年 梁展焯. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
